812a0380dcc5d4ba1893432bf7f11e76f1cea09b
bigmosi <kinyeramo@gmail.com>
2022-08-23 11:43:20 +0300

    fixed ruby error

--- person.rb
+++ person.rb
@@ -1,25 +1,27 @@
-class Person
-  attr_accessor :name, :age
-  attr_reader :id
+# frozen_string_literal: true
 
+# class called person
+class Person
+  attr_reader :id
+  attr_accessor :name, :age
+
+  # initializing the class person
   def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = Random.rand(1..100)
-    @age = age
+    @id = rand(1..1000)
     @name = name
+    @age = age
     @parent_permission = parent_permission
   end
 
-  def can_use_services
-    if of_age? || parent_permission
-      true
-    else
-      false
-    end
-  end
-
-  private
-
+  # Private method is_of_age?
   def of_age?
-    @age > 18
+    @age >= 18
   end
+  private :of_age?
+
+  # Public method can_use_services?
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+  public :can_use_services?
 end
