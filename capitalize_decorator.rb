12e8e2eddb643cb9951c96bd728567e0896e9857
bigmosi <kinyeramo@gmail.com>
2022-08-25 18:43:26 +0300

    worked on adding books to the libraray

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -1,6 +1,11 @@
-require './base_decorator'
+require './nameable'
 
-class CapitalizeDecorator < Decorator
+class CapitalizeDecorator < Nameable
+  def initialize(nameable = Nameable.new)
+    super()
+    @nameable = nameable
+  end
+
   def correct_name
     @nameable.correct_name.capitalize
   end
--- student.rb
+++ student.rb
@@ -1,10 +1,16 @@
-require_relative 'person'
+require './person'
 
-# Class Student inherits from Person
 class Student < Person
-  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+  attr_reader :classroom
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
